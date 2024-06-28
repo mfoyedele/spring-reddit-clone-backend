8f107edd22fc1078db6af4c9fa380ce9d12800d3
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-06-28 10:00:33 +0100

    Add CommentRepository class

---
+++ src/main/java/com/funso/techie/springredditclone/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.funso.techie.springredditclone.repository;
+
+import com.funso.techie.springredditclone.model.Comment;
+import com.funso.techie.springredditclone.model.Post;
+import com.funso.techie.springredditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
+}
