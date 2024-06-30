d7e252ad5360cafe7ef2311e0fb7c6257a5fc4b4
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-06-30 09:15:36 +0100

    Update SecurityConfig class

---
+++ src/main/java/com/funso/techie/springredditclone/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.funso.techie.springredditclone.config;
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
