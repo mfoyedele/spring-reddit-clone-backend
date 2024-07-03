f91118a40f97b055716428044ca803202f8fed6a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-03 11:05:00 +0100

    Update SpringRedditCloneApplication class

--- src/main/java/com/funso/techie/springredditclone/SpringredditcloneApplication.java
+++ src/main/java/com/funso/techie/springredditclone/SpringredditcloneApplication.java
@@ -1,13 +1,18 @@
 package com.funso.techie.springredditclone;
 
+import com.funso.techie.springredditclone.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class SpringredditcloneApplication {
+@EnableAsync
+@Import(OpenAPIConfiguration.class)
+public class SpringRedditCloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringredditcloneApplication.class, args);
+		SpringApplication.run(com.programming.techie.springredditclone.SpringRedditCloneApplication.class, args);
 	}
 
 }
