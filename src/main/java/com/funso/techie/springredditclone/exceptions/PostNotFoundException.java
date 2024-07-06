174f46e9c4acc9a3be46464447a55aa558dc74ff
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-06 22:35:07 +0100

    Update PostNotFoundException class

--- src/main/java/com/funso/techie/springredditclone/exceptions/PostNotFoundException.java
+++ src/main/java/com/funso/techie/springredditclone/exceptions/PostNotFoundException.java
@@ -1,4 +1,6 @@
 package com.funso.techie.springredditclone.exceptions;
 
-public class PostNotFoundException {
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+    }
 }
