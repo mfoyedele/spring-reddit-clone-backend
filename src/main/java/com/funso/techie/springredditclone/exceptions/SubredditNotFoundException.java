10ba6053ab474cfa5bc46dab8a8cc9455ce20630
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-06 22:36:33 +0100

    Update  SubredditNotFoundException class

--- src/main/java/com/funso/techie/springredditclone/exceptions/SubredditNotFoundException.java
+++ src/main/java/com/funso/techie/springredditclone/exceptions/SubredditNotFoundException.java
@@ -1,4 +1,7 @@
 package com.funso.techie.springredditclone.exceptions;
 
-public class SubredditNotFoundException {
+public class SubredditNotFoundException extends RuntimeException {
+    public SubredditNotFoundException(String message) {
+        super(message);
+    }
 }
