a63af6bec17f4545331b5faffe67ad66ebabaa70
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-09 18:19:41 +0100

    Update VoteDto class

--- src/main/java/com/funso/techie/springredditclone/dto/VoteDto.java
+++ src/main/java/com/funso/techie/springredditclone/dto/VoteDto.java
@@ -1,4 +1,14 @@
 package com.funso.techie.springredditclone.dto;
 
+import com.funso.techie.springredditclone.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
 }
