52848f810ad8263e0f706e7d9a908a96c2993460
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-02 16:18:30 +0100

    Implement test for containsSwearWords method

---
+++ src/test/java/com/funso/techie/springredditclone/service/CommentServiceTest.java
@@ -0,0 +1,12 @@
+package com.funso.techie.springredditclone.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CommentServiceTest {
+
+    @Test
+    void containsSwearWords() {
+    }
+}
