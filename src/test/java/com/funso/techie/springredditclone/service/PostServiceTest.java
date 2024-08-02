def60c18ca96f31f6d2f692a7d3a43f07a568b4a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-02 17:43:33 +0100

    Update both PostServiceTest and pom.xml

---
+++ src/test/java/com/funso/techie/springredditclone/service/PostServiceTest.java
@@ -0,0 +1,12 @@
+package com.funso.techie.springredditclone.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    void getPost() {
+    }
+}
