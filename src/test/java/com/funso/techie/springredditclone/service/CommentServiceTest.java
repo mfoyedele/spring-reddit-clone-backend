30ba42d4fe1bc02ba262119d781fc09fd67406ff
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-02 17:00:36 +0100

    Implement test case for containsSwearWords method

--- src/test/java/com/funso/techie/springredditclone/service/CommentServiceTest.java
+++ src/test/java/com/funso/techie/springredditclone/service/CommentServiceTest.java
@@ -1,12 +1,17 @@
 package com.funso.techie.springredditclone.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CommentServiceTest {
 
     @Test
-    void containsSwearWords() {
+    @DisplayName("Test Should Pass When Comment do not Contains Swear Words")
+    void shouldNotContainSwearWordsInsideComment() {
+        CommentService commentService = new CommentService(null, null, null, null, null, null, null);
+        assertThat(commentService.containsSwearWords("This is a comment")).isFalse();
     }
 }
