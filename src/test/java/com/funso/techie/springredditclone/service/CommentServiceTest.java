b22cacbbe97c5ed9b86e8f24754ece2415ef0115
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-02 17:10:04 +0100

    Implement test case for shouldFailWhenCommentContainsSwearWords method

--- src/test/java/com/funso/techie/springredditclone/service/CommentServiceTest.java
+++ src/test/java/com/funso/techie/springredditclone/service/CommentServiceTest.java
@@ -1,9 +1,11 @@
 package com.funso.techie.springredditclone.service;
 
+import com.funso.techie.springredditclone.exceptions.SpringRedditException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CommentServiceTest {
@@ -13,5 +15,16 @@ public class CommentServiceTest {
     void shouldNotContainSwearWordsInsideComment() {
         CommentService commentService = new CommentService(null, null, null, null, null, null, null);
         assertThat(commentService.containsSwearWords("This is a comment")).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should Throw Exception when Exception Contains Swear Words")
+    void shouldFailWhenCommentContainsSwearWords() {
+        CommentService commentService = new CommentService(null, null, null, null, null, null, null);
+
+        assertThatThrownBy(() -> {
+            commentService.containsSwearWords("This is a shitty comment");
+        }).isInstanceOf(SpringRedditException.class)
+                .hasMessage("Comments contains unacceptable language");
     }
 }
