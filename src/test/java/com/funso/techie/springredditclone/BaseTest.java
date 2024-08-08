ef3007128878246c39f8791913d6df33d132123c
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-08 16:44:37 +0100

    Integration test implemented

--- src/test/java/com/funso/techie/springredditclone/BaseTest.java
+++ src/test/java/com/funso/techie/springredditclone/BaseTest.java
@@ -1,16 +1,53 @@
 package com.funso.techie.springredditclone;
 
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
 import org.testcontainers.containers.MySQLContainer;
 
 public abstract class BaseTest {
 
-    static MySQLContainer mySQLContainer = (MySQLContainer) new MySQLContainer("mysql:latest")
-            .withDatabaseName("spring-reddit-test-db")
-            .withUsername("testuser")
-            .withPassword("pass")
-            .withReuse(true);
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>(
+            "mysql:latest"
+    );
 
     static {
-        mySQLContainer.start();
+        mysql.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(mysql::stop));
     }
+
+    public static void configureRestAssured(Integer port) {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+
+//    @BeforeAll
+//    static void beforeAll() {
+//        postgres.start();
+//    }
+
+    @AfterAll
+    static void afterAll() {
+        mysql.stop();
+    }
+
+//    @DynamicPropertySource
+//    static void configureProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+//        registry.add("spring.datasource.username", postgres::getUsername);
+//        registry.add("spring.datasource.password", postgres::getPassword);
+//    }
+
+    @DynamicPropertySource
+    public static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto",() -> "create");
+    }
+
 }
