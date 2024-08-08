dde967d7fcbed678db6c497ece86e9b6c00bb091
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-08 17:04:25 +0100

    Update UserRepositoryTest class

--- src/test/java/com/funso/techie/springredditclone/repository/UserRepositoryTest.java
+++ src/test/java/com/funso/techie/springredditclone/repository/UserRepositoryTest.java
@@ -2,21 +2,37 @@ package com.funso.techie.springredditclone.repository;
 
 import com.funso.techie.springredditclone.BaseTest;
 import com.funso.techie.springredditclone.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@Testcontainers
 public class UserRepositoryTest extends BaseTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @LocalServerPort
+    private Integer port;
+
+
+    @BeforeEach
+    void setUp() {
+        BaseTest.configureRestAssured(port);
+        userRepository.deleteAll();
+    }
 
     @Test
     public void shouldSavePost() {
